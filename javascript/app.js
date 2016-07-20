e9c44906427a1de6aa6d9fe840d1471e46f24ba4
KBrandtjen <Keithbrandtjen09@gmail.com>
2016-07-19 21:39:41 -0700

    add photo and work with formatting

--- javascript/app.js
+++ javascript/app.js
@@ -1,53 +1,53 @@
-var userName = prompt('Hello! What is your name?')
-
-alert('Hello, ' + userName + '! I dont know you, but you\'re about to learn about me!')
-
-
-result = window.confirm('Would you like to continue?')
-
-
-var answer1 = prompt('Do I have an adorable pitbull?')
-console.log(answer1)
-if (answer1.toLowerCase() === 'yes' || answer1.toLowerCase() === 'y') {
-  alert('Correct ' + userName + '! If you want proof, I\'d be happy to show you!');
-}  else if (answer1.toLowerCase() === 'no' || answer1.toLowerCase() === 'n') {
-    alert('Sorry ' + userName + '! I do and I have 1000\'s of photos to prove it!');
-}
-else { alert('Try typing yes or no ( y/n ).');
-}
-
-
-var answer2 = prompt('Can I do a flawless Homer Simpson impersonation?');
-if (answer2.toLowerCase() === 'no' || answer2.toLowerCase() === 'n') {
-  alert('Correct ' + userName + '! But my Bart isn\'t too bad.');
-}
-else {
-  alert('D\'OH!! Sorry ' + userName + '. I can\'t but I enjoy the occasional Duff.');
-}
-
-
-var answer3 = prompt('Can I identify the species of every shrub and tree outside right now?');
-if (answer3.toLowerCase() === 'yes' || answer3.toLowerCase() === 'y') {
-  alert('Indeed! I love plants!');
-}
-else {
-  alert('Incorrect, Acer saccharum, Fagus sylvatica, Quercus robur.......');
-}
-
-
-var answer4 = prompt('Is my favorite type of music Hip-Hop?');
-if (answer4.toLowerCase() === 'yes' || answer4.toLowerCase() === 'y') {
-  alert('For shizzle, ' + userName + '!');
-}
-else {
-  alert('Chickity check yourself ' + userName +'! You\'re about to wreck yourself!');
-}
-
-
-var answer5 = prompt('Have I have been coding for 3 years?');
-if (answer5.toLowerCase() === 'no' || answer5.toLowerCase() === 'n') {
-  alert('I have not, but you wouldn\'t know it from this website!');
-}
-else {
-  alert('Sorry ' + userName + ', I have not but thanks for the vote of confidence!');
-}
+// var userName = prompt('Hello! What is your name?')
+//
+// alert('Hello, ' + userName + '! I dont know you, but you\'re about to learn about me!')
+//
+//
+// result = window.confirm('Would you like to continue?')
+//
+//
+// var answer1 = prompt('Do I have an adorable pitbull?')
+// console.log(answer1)
+// if (answer1.toLowerCase() === 'yes' || answer1.toLowerCase() === 'y') {
+//   alert('Correct ' + userName + '! If you want proof, I\'d be happy to show you!');
+// }  else if (answer1.toLowerCase() === 'no' || answer1.toLowerCase() === 'n') {
+//     alert('Sorry ' + userName + '! I do and I have 1000\'s of photos to prove it!');
+// }
+// else { alert('Try typing yes or no ( y/n ).');
+// }
+//
+//
+// var answer2 = prompt('Can I do a flawless Homer Simpson impersonation?');
+// if (answer2.toLowerCase() === 'no' || answer2.toLowerCase() === 'n') {
+//   alert('Correct ' + userName + '! But my Bart isn\'t too bad.');
+// }
+// else {
+//   alert('D\'OH!! Sorry ' + userName + '. I can\'t but I enjoy the occasional Duff.');
+// }
+//
+//
+// var answer3 = prompt('Can I identify the species of every shrub and tree outside right now?');
+// if (answer3.toLowerCase() === 'yes' || answer3.toLowerCase() === 'y') {
+//   alert('Indeed! I love plants!');
+// }
+// else {
+//   alert('Incorrect, Acer saccharum, Fagus sylvatica, Quercus robur.......');
+// }
+//
+//
+// var answer4 = prompt('Is my favorite type of music Hip-Hop?');
+// if (answer4.toLowerCase() === 'yes' || answer4.toLowerCase() === 'y') {
+//   alert('For shizzle, ' + userName + '!');
+// }
+// else {
+//   alert('Chickity check yourself ' + userName +'! You\'re about to wreck yourself!');
+// }
+//
+//
+// var answer5 = prompt('Have I have been coding for 3 years?');
+// if (answer5.toLowerCase() === 'no' || answer5.toLowerCase() === 'n') {
+//   alert('I have not, but you wouldn\'t know it from this website!');
+// }
+// else {
+//   alert('Sorry ' + userName + ', I have not but thanks for the vote of confidence!');
+// }
