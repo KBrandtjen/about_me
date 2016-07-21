07406fd86f0a1ca154fdb535f04714f28e9dfe00
KBrandtjen <Keithbrandtjen09@gmail.com>
2016-07-21 08:56:00 -0700

    edited syntax

--- javascript/app.js
+++ javascript/app.js
@@ -7,7 +7,7 @@ alert('Hello, ' + userName + '! I dont know you, but you\'re about to learn abou
 
 
 var answer1 = prompt('Do I have an adorable pitbull?')
-console.log(answer1)
+console.log(answer1);
 
 if (answer1.toLowerCase() === 'yes' || answer1.toLowerCase() === 'y') {
   alert('Correct ' + userName + '! If you want proof, I\'d be happy to show you!');
@@ -21,7 +21,7 @@ if (answer1.toLowerCase() === 'yes' || answer1.toLowerCase() === 'y') {
 
 
 var answer2 = prompt('Can I do a flawless Homer Simpson impersonation?');
-console.log(answer2)
+console.log(answer2);
 
 if (answer2.toLowerCase() === 'no' || answer2.toLowerCase() === 'n') {
   alert('Correct ' + userName + '! But my Bart isn\'t too bad.');
@@ -34,7 +34,7 @@ if (answer2.toLowerCase() === 'no' || answer2.toLowerCase() === 'n') {
 
 
 var answer3 = prompt('Can I identify the species of every shrub and tree outside right now?');
-console.log(answer3)
+console.log(answer3);
 
 if (answer3.toLowerCase() === 'yes' || answer3.toLowerCase() === 'y') {
   alert('Indeed! I love plants!');
@@ -47,7 +47,7 @@ if (answer3.toLowerCase() === 'yes' || answer3.toLowerCase() === 'y') {
 
 
 var answer4 = prompt('Is my favorite type of music Hip-Hop?');
-console.log(answer4)
+console.log(answer4);
 
 if (answer4.toLowerCase() === 'yes' || answer4.toLowerCase() === 'y') {
   alert('For shizzle, ' + userName + '!');
@@ -60,7 +60,7 @@ if (answer4.toLowerCase() === 'yes' || answer4.toLowerCase() === 'y') {
 
 
 var answer5 = prompt('Have I have been coding for 3 years?');
-console.log(answer5)
+console.log(answer5);
 
 if (answer5.toLowerCase() === 'no' || answer5.toLowerCase() === 'n') {
   alert('I have not, but you wouldn\'t know it from this website!');
@@ -77,7 +77,7 @@ var magicNumber = 44;
 var i = 0
 while (i < 4) {
   var answer6 = prompt('Can you guess what number I\'m thinking of between 0 and 100');
-  // console.log(answer6);
+  console.log(answer6);
   i++;
 if (isNaN(answer6)) {
   alert ('Please enter a number!');
@@ -101,6 +101,7 @@ var guesses = 0;
 while (guesses < 7) {
   var choice7 = prompt('Can you guess any of my favorite hip-hop artists?');
   var answer7 = choice7.toLowerCase();
+  console.log(answer7);
   for(var j = 0; j < favoriteRap.length; j++) {
     var arrayElement = favoriteRap[j];
     if (arrayElement === answer7) {
@@ -119,7 +120,9 @@ while (guesses < 7) {
 };
   guesses++;
 };
+
 alert('Want to know who else is in my top 7? Kendrick Lamar, Outkast, Schoolboy Q, Chance the Rapper, Nas, Kid Cudi, and Anderson .Paak. Give them a listen!');
+
 
 var total = score1 + score2 + score3 + score4 + score5 + score6 + score7;
 
